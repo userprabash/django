0a1b2b28eadc0d09a4f6092067e202632d7bfdf7
userprabash <prabashnepal160@gmail.com>
2025-02-26 16:01:28 +0545

    Your commit message describing the changes

--- tweetapp/tweetapp/urls.py
+++ tweetapp/tweetapp/urls.py
@@ -4,8 +4,12 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth.urls import views as auth_views
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+   path('',views.layout, name='layout' ),
+    
     path('tweets/',include('tweets.urls')),
     path('accounts/',include('django.contrib.auth.urls'))
 
--- tweetapp/tweets/forms.py
+++ tweetapp/tweets/forms.py
@@ -9,6 +9,7 @@ class TweetForm(forms.ModelForm):
 
 class UserRegistrationForm(UserCreationForm):
     email=forms.EmailField()
+    
     class Meta:
         model=User
         fields=('username','email','password1','password2')
